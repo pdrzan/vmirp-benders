9824c28a0f8f991ea1d6cbdde8e221fcd4aa780e
pdrzan <pedeozanelato19@gmail.com>
2025-11-05 08:54:28 -0300

    feat(main.py): add passing instance by terminal

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
+import sys
 from instance import read_instance
 
 
 def main():
-    file_path = "instances/Instances_highcost_H3/abs1n10.dat"
-    data = read_instance(file_path)
+    description = (
+        "This program resolves the VM-IRP problem with benders "
+        "decomposition. To run, you have to pass the following "
+        "arguments: "
+        "- '-i': File path of the VM-IRP instance"
+        "\n"
+        "Example:\n"
+        "src/main.py -i instances/Instances_highcost_H3/abs1n10.dat\n"
+    )
+
+    arguments = {"-i": None}
+
+    for index_argument, argument in enumerate(sys.argv):
+        if argument in arguments and index_argument + 1 < len(sys.argv):
+            arguments[argument] = sys.argv[index_argument + 1]
+
+    for value in arguments.values():
+        if value is None:
+            raise Exception("Parameters not defined!\n" + description)
+
+    instance_file_path = arguments["-i"]
+    data = read_instance(instance_file_path)
 
 
 if __name__ == "__main__":
