5e512a72dfb95bd7e15c69c897e75b9625fd2fb9
pdrzan <pedeozanelato19@gmail.com>
2025-11-06 19:57:29 -0300

    feat(instance.py): add distance matrix calculation in data

--- src/instance.py
+++ src/instance.py
@@ -1,4 +1,9 @@
 import os
+import math
+
+
+def distance_between(x_i, x_j, y_i, y_j):
+    return round(math.sqrt((x_i - x_j) ** 2 + (y_i - y_j) ** 2))
 
 
 def read_instance(file_path):
@@ -11,13 +16,13 @@ def read_instance(file_path):
 
         data["n"], data["H"], data["C"] = map(int, file.readline().split())
 
-        index, x, y, B_1, r_1, h_1 = file.readline().split()
+        index, x, y, B_0, r_0, h_0 = file.readline().split()
         data["supplier"]["index"] = int(index)
         data["supplier"]["x"] = float(x)
         data["supplier"]["y"] = float(y)
-        data["supplier"]["B_1"] = int(B_1)
-        data["supplier"]["r_1"] = int(r_1)
-        data["supplier"]["h_1"] = float(h_1)
+        data["supplier"]["B_0"] = int(B_0)
+        data["supplier"]["r_0"] = int(r_0)
+        data["supplier"]["h_0"] = float(h_0)
 
         for _ in range(data["n"] - 1):
             index, x, y, I_i0, U_i, L_i, r_i, h_i = file.readline().split()
@@ -33,5 +38,22 @@ def read_instance(file_path):
                     "h_i": float(h_i),
                 }
             )
+
+        data["distance_matrix"] = []
+
+        for customer_i in [data["supplier"]] + data["customers"]:
+            distance_row = []
+
+            for customer_j in [data["supplier"]] + data["customers"]:
+                distance_row.append(
+                    distance_between(
+                        customer_i["x"],
+                        customer_j["x"],
+                        customer_i["y"],
+                        customer_j["y"],
+                    )
+                )
+
+            data["distance_matrix"].append(distance_row)
 
     return data
