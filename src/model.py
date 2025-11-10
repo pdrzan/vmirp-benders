1047ed44d0b0de84456dea5e5e566da0b78e4fd4
Francisco Neto <limanetoff@gmail.com>
2025-11-09 22:06:01 -0300

    feat: MTZ constraints for subproblems LB

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ import math
 from concorde.tsp import TSPSolver
 
 solved_bsp = {}
+solved_list = []
 
 
 def optimize(data):
@@ -11,6 +12,22 @@ def optimize(data):
     time_horizon_ = range(data["H"] + 2)
     customers = data["customers"]
     supplier = data["supplier"]
+
+    nodes = [1] + [customer["index"] for customer in customers]
+
+    def get_dist(x_1, y_1, x_2, y_2):
+        return math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
+
+    dist = [[0 for _ in range(len(customers)+1)] for _ in range(len(customers)+1)]
+
+    for i in range(len(customers)):
+        dist[i+1][0] = int(get_dist(supplier["x"], supplier["y"], customers[i]["x"], customers[i]["y"]))
+        dist[0][i+1] = dist[i+1][0]
+    
+    for i in range(len(customers)):
+        for j in range(len(customers)):
+            dist[i+1][j+1] = int(get_dist(customers[i]["x"], customers[i]["y"], customers[j]["x"], customers[j]["y"]))
+            dist[j+1][i+1] = dist[i+1][j+1]
 
     model = gp.Model()
 
@@ -21,7 +38,7 @@ def optimize(data):
     )
 
     z_st = model.addVars(
-        [customer["index"] for customer in customers],
+        nodes,
         [time for time in time_horizon],
         vtype=gp.GRB.BINARY,
         name="z_st",
@@ -45,6 +62,21 @@ def optimize(data):
         [time for time in time_horizon],
         vtype=gp.GRB.CONTINUOUS,
         name="eta_t",
+    )
+
+    v_ijt = model.addVars(
+        nodes,
+        nodes,
+        [time for time in time_horizon],
+        vtype=gp.GRB.CONTINUOUS,
+        name="v_ijt",
+    )
+
+    u_it = model.addVars(
+        [customer["index"] for customer in customers],
+        [time for time in time_horizon],
+        vtype=gp.GRB.CONTINUOUS,
+        name="u_it",
     )
 
     model.setObjective(
@@ -79,6 +111,16 @@ def optimize(data):
             == 0
         ),
         name="initial_visits",
+    )
+
+    model.addConstrs(
+        (
+            z_st[1, time]
+            >= z_st[customer_index, time]
+            for customer_index in [customer["index"] for customer in customers]
+            for time in time_horizon 
+        ),
+        name="supplier_moviments",
     )
 
     model.addConstrs(
@@ -180,8 +222,68 @@ def optimize(data):
         name="vehicle_capacity",
     )
 
-    def get_dist(x_1, y_1, x_2, y_2):
-        return math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
+    model.addConstrs(
+        (
+            gp.quicksum(v_ijt[i, j, time] for j in nodes)
+            == z_st[i, time]
+            for i in nodes
+            for time in time_horizon
+        ),
+        name="a",
+    )
+
+    model.addConstrs(
+        (
+            gp.quicksum(v_ijt[i, j, time] for i in nodes)
+            == z_st[j, time]
+            for j in nodes
+            for time in time_horizon
+        ),
+        name="b",
+    )
+
+    model.addConstrs(
+        (
+            u_it[customer_index, time]
+            >= 2 * z_st[customer_index, time]
+            for customer_index in [customer["index"] for customer in customers]
+            for time in time_horizon
+        ),
+        name="c",
+    )
+
+    model.addConstrs(
+        (
+            u_it[customer_index, time] <= gp.quicksum(z_st[i, time] for i in nodes)
+            for customer_index in [customer["index"] for customer in customers]
+            for time in time_horizon
+        ),
+        name="d",
+    )
+
+    model.addConstrs(
+        (
+            u_it[i, time] - u_it[j, time] + 1
+            <= (len(customers) + 1) * (1 - v_ijt[i, j, time])
+            for i in [customer["index"] for customer in customers]
+            for j in [customer["index"] for customer in customers]
+            for time in time_horizon
+        ),
+        name="a",
+    )
+
+    model.addConstrs(
+        (
+            eta_t[time]
+            >= gp.quicksum(
+                dist[i-1][j-1]
+                * v_ijt[i, j, time]
+                for i in nodes
+                for j in nodes if i!=j)
+            for time in time_horizon
+        ),
+        name="e",
+    )
 
     def benders_cuts(model, where):
         if where == gp.GRB.Callback.MIPSOL:
@@ -201,7 +303,7 @@ def optimize(data):
 
                 tour_hash = '.'.join([str(node) for node in visited])
                 if tour_hash in solved_bsp:
-                    bsd_cost = solved_bsp[tour_hash][-1]
+                    bsd_cost = solved_bsp[tour_hash]
 
                 elif len(xs) < 2:
                     continue
@@ -220,7 +322,11 @@ def optimize(data):
                     bsd_sol = bsd_solver.solve(verbose=False)
                     bsd_cost = bsd_sol.optimal_value
 
-                    solved_bsp[tour_hash] = bsd_sol.tour.tolist() + [bsd_cost]
+                    solved_bsp[tour_hash] = bsd_cost
+                    solved_list.append(tour_hash)
+                    if len(solved_list) > 10000:
+                        removed_sol = solved_list.pop(0)
+                        del solved_bsp[removed_sol]
 
                 if eta < bsd_cost:
                     for _time in time_horizon:
